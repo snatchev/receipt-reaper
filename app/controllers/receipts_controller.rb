bf7d195ffc0627a8d7e748d89a4859e14ec580de
Stefan Natchev <stefan.natchev@gmail.com>
2011-07-17 09:52:08 -0400

    add edit and delete actions

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -14,4 +14,26 @@ class ReceiptsController < ApplicationController
     end
   end
 
+  def edit
+    @receipt = Receipt.find(params[:id])
+    @receipts = Receipt.all
+    render :action => 'index'
+  end
+
+  def update
+    @receipt = Receipt.find(params[:id])
+    if @receipt.update_attributes(params[:receipt])
+      redirect_to receipts_path
+    else
+      @receipts = Receipt.all
+      render :action => 'index'
+    end
+  end
+
+  def destroy
+    @receipt = Receipt.find(params[:id])
+    @receipt.destroy
+    redirect_to receipts_path
+  end
+
 end

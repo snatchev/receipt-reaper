6992c9caff57fccf2b8650fe648b641474c40e57
Stefan Natchev <stefan.natchev@gmail.com>
2011-09-05 21:13:39 -0400

    add some basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :authenticate
+  private
+    def authenticate
+      authenticate_or_request_with_http_basic do |user_name, password|
+        user_name == USER_NAME && password == PASSWORD
+      end
+    end
 end
